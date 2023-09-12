0b6b53613c9a29c2c41c5ead91b6f34bbbcc8a07
rsveinson <rsveinson@mts.net>
2023-09-12 09:51:30 -0500

    added file i/0 and eof loop

---
+++ CS30SEOFReview.java
@@ -0,0 +1,83 @@
+import java.util.Scanner;
+import javax.swing.*;
+import java.io.*;
+import java.text.NumberFormat;
+
+/** ***************************************************
+ *  Name:           Sveinson
+ *  Class:          CS30S
+ * 
+ *  Assignment:     EOF loop review
+ * 
+ *  Description:    a basic review of concepts from cs20s
+ *                  starting with EOF loops and external data
+ *************************************************************/
+
+public class CS30SEOFReview {
+
+    public static void main(String[] args) throws IOException{
+    // ***** constants *******
+    
+    // ***** variables *****
+    
+        String banner = "";             // output banner
+        String prompt = "";             // prompt for user input
+        
+        String strin = "";              // string fro keyboard input
+        String strout = "";             // string for formatted output
+        
+        String delim = "[ ]+";          // delimiter for splitting input records
+        String[] tokens = null;         // used to split input records
+        
+        // a new line character that works on every computer system
+        String nl = System.lineSeparator();
+    
+    // ***** objects *****
+    
+        //Scanner scanner = new Scanner(System.in);
+        //NumberFormat currency = NumberFormat.getCurrencyInstance();
+        
+        // file io buffers for reading and writing to text files
+        
+        BufferedReader fin = new BufferedReader(new FileReader("CS30SEOFReviewData.txt"));
+        //PrintWriter fout = new PrintWriter(new BufferedWriter(new FileWriter("outfle.txt")));
+    
+    // ***** print banners *****
+    
+        banner = "*****************************" + nl;
+        banner += "Name:        Sveinson" + nl;
+        banner += "Class:       CS30S" + nl;
+        banner += "Assignment:  EOF Review" + nl;
+        banner += "*****************************" + nl + nl;
+        
+        System.out.println(banner);
+        //fout.print(banner);
+    
+    // ***** Get Input *****  
+    // ***** Main Processing *****
+    
+        strin = fin.readLine();
+        //System.out.println(strin);
+        
+        while(strin != null){
+            System.out.println(strin);
+            
+            // update the loop control
+            strin = fin.readLine();
+        }// end eof
+    
+    // ***** Print Formatted Output *****
+    
+    // ***** Closing Message *****
+    
+        System.out.println();
+        System.out.println("end of processing");
+        //fout.println("End of Processing");
+        
+    // **** close io buffers *****
+    
+        fin.close();
+        //fout.close();
+    } // end main 
+    
+} // end FormatTemplate
